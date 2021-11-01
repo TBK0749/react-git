2c7986ca0878991d1fe0d086fc9f7ed144f7709b
Boontharik Keawbenjanukun <TheBaka_0749@hotmail.com>
2021-11-01 17:57:01 +0700

    การบ้านเสร็จหมดละ สบายใจละ

--- src/pages/StudentDetails.js
+++ src/pages/StudentDetails.js
@@ -1,10 +1,17 @@
 import { useParams } from "react-router";
+import { students } from "../data/students";
 
 export default function StudentDetails() {
   const { id } = useParams();
+  const student = students[Number(id - 1)];
 
+  if (!student) return <div>student not found</div>;
 
   return (
-    <div>Student Details: {id}</div>
+    <div>
+      <h1>{`No.${student.id} ${student.name}`}</h1>
+      <h2>Bio</h2>
+      <p>{student.bio}</p>
+    </div>
   );
 }
